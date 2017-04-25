8d50a301c57b75ff61bd50bbfb9cb74152c2111b
Hannah Nordgren <hey@hereshannahs.info>
2017-04-25 16:38:11 -0400

    [STYLE] started implementing base styles for newsfeed

--- app.js
+++ app.js
@@ -4,15 +4,19 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var hbs = require('hbs');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
 
+hbs.registerPartials(__dirname + '/views/partials');
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
+
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,20 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
+
+/* More on this concept here:  http://stackoverflow.com/questions/35473025/how-to-let-external-json-for-jade-to-render-from-express/35473162 */
+
+var renderFromJson = function(json_path, renderTemplate) {
+    fs.readFile(json_path, function(err, data) {
+        renderTemplate(JSON.parse(data));
+    })
+}
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { intro: {
-    "title": "Listicle",
-    "subtitle": "Here is a list"
-  }}
-);
+  renderFromJson('app/js/article.json', function(json_data) {
+    res.render('index', json_data);
+  })
 });
 
 module.exports = router;
