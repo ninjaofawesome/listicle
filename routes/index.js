e5a89b52694d7e82ed9df0e47652cd16715ce2ba
Hannah Nordgren <hey@hereshannahs.info>
2017-04-25 10:55:02 -0400

    [WIP] added object to test in index.js, works

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Listicle' });
+  res.render('index', { intro: {
+    "title": "Listicle",
+    "subtitle": "Here is a list"
+  }}
+);
 });
 
 module.exports = router;
