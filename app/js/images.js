9e358d9597d8311884caa8b0f41e671614f379b1
Hannah Nordgren <hey@hereshannahs.info>
2017-04-26 11:45:01 -0400

    [CLEANUP/MISC] removed debug-log, committing other files from last push

---
+++ app/js/images.js
@@ -0,0 +1,7 @@
+var $ = require('jquery');
+
+module.export = function() {
+  console.log('images js loaded');
+  // todo:  Hook this up, then select multiple dom elements to manipulate on large or small dom elements.
+
+}
